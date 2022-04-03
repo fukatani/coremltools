dab3ee1ffc17665af6864829f8c8901130187653
Unknown <nannyakannya@gmail.com>
2022-04-03 21:58:51 +0900

    oops

--- coremltools/converters/mil/frontend/torch/test/test_torch_ops.py
+++ coremltools/converters/mil/frontend/torch/test/test_torch_ops.py
@@ -37,65 +37,10 @@ COMMON_SHAPES_ALL = [(1, )] + COMMON_SHAPES
 
 class TestElementWiseUnary(TorchBaseTest):
     @pytest.mark.parametrize(
-        "backend, shape, op_string",
-        itertools.product(
-            backends,
-            [(1, 3, 5, 8)],
-            [
-                "abs",
-                "acos",
-                "asin",
-                "atan",
-                "ceil",
-                "cos",
-                "cosh",
-                "exp",
-                "floor",
-                "round",
-                "sin",
-                "sinh",
-                "sqrt",
-                "square",
-                "tan",
-                "tanh",
-                "sign",
-            ],
-        ),
-    )
-    def test_elementwise_no_params(self, backend, shape, op_string):
-        if not contains_op(torch, op_string):
-            return
-        op_func = getattr(torch, op_string)
-        model = ModuleWrapper(function=op_func)
-        self.run_compare_torch(
-            shape, model, backend=backend,
-        )
-
-    @pytest.mark.parametrize(
-        "backend, shape, clamp_range",
-        itertools.product(
-            backends,
-            [(1, 3, 5, 8)],
-            [(0.0, 1.0), (-1.0, 0.5), (0.2, 0.7), (None, 4.0), (-3.0, None)],
-        ),
-    )
-    def test_clamp(self, backend, shape, clamp_range):
-        params_dict = {}
-        if clamp_range[0] is not None:
-            params_dict["min"] = clamp_range[0]
-        if clamp_range[1] is not None:
-            params_dict["max"] = clamp_range[1]
-
-        model = ModuleWrapper(torch.clamp, params_dict)
-        self.run_compare_torch(
-            shape, model, backend=backend, rand_range=(-5, 5)
-        )
-
-    @pytest.mark.parametrize(
         "backend, shape, diagonal",
         itertools.product(
             backends,
-            [(1, 3, 5, 8)],
+            [((2, 3), (3, 1), (1, 4), (5, 8))],
             [None, 1, 3],
         ),
     )
@@ -106,55 +51,4 @@ class TestElementWiseUnary(TorchBaseTest):
         model = ModuleWrapper(function=torch.triu)
         self.run_compare_torch(
             shape, model, backend=backend,
-        )
-
-    @pytest.mark.parametrize(
-        "backend, shape, diagonal",
-        itertools.product(
-            backends,
-            [(1, 3, 5, 8)],
-            [None, 1],
-        ),
-    )
-    def test_tril(self, backend, shape, diagonal):
-        params_dict = {}
-        if diagonal is not None:
-            params_dict["diagonal"] = diagonal
-        model = ModuleWrapper(function=torch.tril)
-        self.run_compare_torch(
-            shape, model, backend=backend,
-        )
-    @pytest.mark.parametrize(
-        "backend, shape, threshold",
-        itertools.product(
-            backends,
-            [(1, 3, 5, 8)],
-            [(0.0, 0.0), (0.5, 0.5), (0.5, 10), (0.9, 0.0)]
-        ),
-    )
-
-    def test_threshold(self, backend, shape, threshold):
-        model = torch.nn.Threshold(threshold[0], threshold[1]).eval()
-        self.run_compare_torch(
-            shape, model, backend=backend,
-            use_cpu_for_conversion=True, # TODO: change this to False (rdar://78343191)
-        )
-
-    @pytest.mark.parametrize(
-        "backend, shape, op_string",
-        itertools.product(
-            backends,
-            [(1, 3, 5, 8)],
-            [
-                "log",
-                "rsqrt",
-                "reciprocal",
-            ],
-        ),
-    )
-    def test_elementwise_numerically_stable(self, backend, shape, op_string):
-        op_func = getattr(torch, op_string)
-        model = ModuleWrapper(function=op_func)
-        self.run_compare_torch(
-            shape, model, backend=backend, rand_range=(20, 100)
         )
